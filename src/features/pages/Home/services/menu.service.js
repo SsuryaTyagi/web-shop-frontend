b1917594f57a03406092d07b59814e368589bc56
SsuryaTyagi <2040surya@gmail.com>
2026-08-05 14:58:29 +0530

    refactor: Replace axios calls with api instance for menu service

--- src/features/pages/Home/services/menu.service.js
+++ src/features/pages/Home/services/menu.service.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { BASE_URL } from "../../../Api";
 
+const api = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
 export const fetchMenu = async (category) => {
   try {
-    const res = await axios.get(`${BASE_URL}/api/menu?category=${category}`);
+    const res = await api.get(`/api/menu?category=${category}`);
     return res.data;
   } catch (error) {
     throw error.response?.data || error.message;
@@ -11,7 +16,7 @@ export const fetchMenu = async (category) => {
 };
 export const fetchMenuItem = async () => {
   try {
-    const res = await axios.get(`${BASE_URL}/api/categories`);
+    const res = await api.get(`/api/categories`);
     return res.data;    
   } catch (error) {
     console.error("Menu fetch error:", error);
@@ -21,7 +26,7 @@ export const fetchMenuItem = async () => {
 
 export const fetchPopular = async (popular) => {
   try {
-    const res = await axios.get(`${BASE_URL}/api/menu?popular=${popular}`);
+    const res = await api.get(`/api/menu?popular=${popular}`);
     return res.data;
   } catch (error) {
     console.error("Best fetch error:", error);
